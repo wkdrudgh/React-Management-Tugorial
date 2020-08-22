e595aada66e5f0e045305c7c0cb15a09cbe8ef6c
wkdrudgh <boo04178@naver.com>
2020-08-22 21:36:38 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,41 +1,72 @@
-import React, {Component} from 'react';
-import Customer from './components/Customer';
-import './App.css';
+import React, { Component } from "react";
+import Customer from "./components/Customer";
+import "./App.css";
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableHeader from "@material-ui/core/TableHead";
+import TableBody from "@material-ui/core/TableBody";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import { withStyles} from "@material-ui/core/styles";
+
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 
 const customers = [
-{
-  'id':'1',
-  'image':'https://placeimg.com/64/64/1',
-  'name':'장경민',
-  'birthday':'961222',
-  'gender':'남자',
-  'job':'대학생'
-},
-{
-  'id':'2',
-  'image':'https://placeimg.com/64/64/2',
-  'name':'장경',
-  'birthday':'961222',
-  'gender':'남자',
-  'job':'대학생'
-},
-{
-  'id':'3',
-  'image':'https://placeimg.com/64/64/3',
-  'name':'장',
-  'birthday':'961222',
-  'gender':'남자',
-  'job':'대학생'
-},
-]
+  {
+    id: "1",
+    image: "https://placeimg.com/64/64/1",
+    name: "장경민",
+    birthday: "961222",
+    gender: "남자",
+    job: "대학생",
+  },
+  {
+    id: "2",
+    image: "https://placeimg.com/64/64/2",
+    name: "장경",
+    birthday: "961222",
+    gender: "남자",
+    job: "대학생",
+  },
+  {
+    id: "3",
+    image: "https://placeimg.com/64/64/3",
+    name: "장",
+    birthday: "961222",
+    gender: "남자",
+    job: "대학생",
+  },
+];
 
 class App extends Component {
-  render(){
-    return(
-      <div>
-        {
-          customers.map(c => {
-            return(
+  render() {
+    const {classes} = this.props;
+    return (
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHeader>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+          {customers.map((c) => {
+            return (
               <Customer
                 key={c.id}
                 id={c.id}
@@ -46,11 +77,13 @@ class App extends Component {
                 job={c.job}
               />
             );
-          })
-        }
-      </div>
+          })}
+          </TableBody>
+        </Table>
+      </Paper>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
+
