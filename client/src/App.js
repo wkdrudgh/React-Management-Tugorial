8229975459a012aa0e5fe525e67bc7577012e49f
wkdrudgh <boo04178@naver.com>
2020-09-03 12:50:13 +0900

    Add Customer Delete Module

--- client/src/App.js
+++ client/src/App.js
@@ -96,12 +96,14 @@ class App extends Component {
                 <TableCell>생년월일</TableCell>
                 <TableCell>성별</TableCell>
                 <TableCell>직업</TableCell>
+                <TableCell>설정</TableCell>
               </TableRow>
             </TableHeader>
             <TableBody>
             {this.state.customers ? this.state.customers.map((c) => {
               return (
                 <Customer
+                  stateRefresh={this.stateRefresh}
                   key={c.id}
                   id={c.id}
                   image={c.image}
